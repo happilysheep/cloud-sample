14de5d7c63011957f7e4e895b45bd99da4ae8c02
wzj <wangzj91@sina.cn>
2018-07-11 15:11:51 +0800

    first commit

---
+++ sample-common/src/main/java/com/sample/common/util/MessageConstants.java
@@ -0,0 +1,31 @@
+package com.sample.common.util;
+
+public enum MessageConstants {
+    NotFound(404,"service is not found");
+
+    private int code;
+
+    private String Msg;
+
+    MessageConstants(int code, String msg) {
+        this.code = code;
+        Msg = msg;
+    }
+
+    public int getCode() {
+
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return Msg;
+    }
+
+    public void setMsg(String msg) {
+        Msg = msg;
+    }
+}
